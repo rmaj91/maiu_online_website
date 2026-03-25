54b2888db9186929c2c64744c5ca410f4f5b2536
rmaj91 <rmaj91@gmail.com>
2026-03-25 21:59:12 +0100

    script

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 console.log('SCRIPT IS WORKING')
+
+function updateVisibility() {
+    document.addEventListener("contextmenu", event => event.preventDefault());
+
+    const isMobile = window.innerWidth <= 800 || window.innerHeight <= 540;
+
+    const hiddenOnMobile = document.querySelectorAll('.mobile-hide');
+    const shownOnMobile = document.querySelectorAll('.mobile-show');
+
+    hiddenOnMobile.forEach(el => {
+        el.classList.toggle('hidden', isMobile);
+    });
+
+    shownOnMobile.forEach(el => {
+        el.classList.toggle('hidden', !isMobile);
+    });
+
+}
+
+window.addEventListener('resize', updateVisibility);
+window.addEventListener('DOMContentLoaded', updateVisibility);
